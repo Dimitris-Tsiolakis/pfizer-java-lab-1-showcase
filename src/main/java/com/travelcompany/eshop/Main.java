3bf0ddd5af98d3b900d17268864971d8770506e0
Ioannis Daniil <jondan97@gmail.com>
2022-09-13 11:56:38 +0300

    starting step

---
+++ src/main/java/com/travelcompany/eshop/Main.java
@@ -0,0 +1,13 @@
+package com.travelcompany.eshop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("hello world");
+    }
+}
