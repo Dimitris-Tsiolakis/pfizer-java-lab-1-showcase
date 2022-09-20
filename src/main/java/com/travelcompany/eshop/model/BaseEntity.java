7bd1e1d4149dd2971af5adb175183829e14558a6
Ioannis Daniil <jondan97@gmail.com>
2022-09-20 13:27:59 +0300

    added "dummy" way for ID generation

--- src/main/java/com/travelcompany/eshop/model/BaseEntity.java
+++ src/main/java/com/travelcompany/eshop/model/BaseEntity.java
@@ -2,6 +2,11 @@ package com.travelcompany.eshop.model;
 
 public abstract class BaseEntity {
     private long id;
+    private static int counter = 0;
+
+    public BaseEntity() {
+        this.id = ++counter;
+    }
 
     public long getId() {
         return id;
