95782f8ab1a9404274fd06b619fdb3e70e5e6374
Ioannis Daniil <jondan97@gmail.com>
2022-09-20 13:16:28 +0300

    added example headers and how they would be applied within an excel service

--- src/main/java/com/travelcompany/eshop/service/excel/CustomerExcelService.java
+++ src/main/java/com/travelcompany/eshop/service/excel/CustomerExcelService.java
@@ -1,9 +1,12 @@
 package com.travelcompany.eshop.service.excel;
 
 import com.travelcompany.eshop.model.Customer;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,10 +16,17 @@ import java.util.List;
 
 public class CustomerExcelService implements AbstractExcelService<Customer> {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomerExcelService.class);
+    // example headers and how they would be applied within an Excel file
+    private final String[] headers = {"ID", "Name", "Email", "Address", "Nationality", "Category",};
+
+
     @Override
     public List<Customer> readFromFile(String filePath) {
         try(FileInputStream fileInputStream = new FileInputStream(filePath);
             XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream)){
+            logger.info("Writing multiple customers to Excel");
+
             final XSSFSheet sheet = workbook.getSheetAt(0);
 
             final XSSFRow row = sheet.getRow(0);
@@ -36,6 +46,15 @@ public class CustomerExcelService implements AbstractExcelService<Customer> {
             XSSFSheet sheet = workbook.createSheet("Customers");
 
             int rowIndex = 0;
+            Row header = sheet.createRow(rowIndex++);
+            header.createCell(0).setCellValue(headers[0]);
+            header.createCell(1).setCellValue(headers[1]);
+            header.createCell(2).setCellValue(headers[2]);
+            header.createCell(3).setCellValue(headers[3]);
+            header.createCell(4).setCellValue(headers[4]);
+            header.createCell(5).setCellValue(headers[5]);
+
+
             for (Customer customer : customers) {
                 XSSFRow row = sheet.createRow(rowIndex++);
                 row.createCell(0).setCellValue(customer.getName()); //A
@@ -49,7 +68,7 @@ public class CustomerExcelService implements AbstractExcelService<Customer> {
 
             return true;
         } catch (IOException ioException) {
-            ioException.printStackTrace();
+            logger.error("", ioException);
             return false;
         }
     }
